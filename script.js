ce24290dc56c8deae10a3800f7521a802bb06ab6
Ossie <osvald.rye@gmail.com>
2023-08-24 11:37:32 +0200

    changes?

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const article1 = document.querySelector("article:first-child")
-const article1P = document.querySelector("article:first-child p")
-const article2P = document.querySelector("article:last-child p")
-const article2LastP = document.querySelector("article:last-child p:last-child")
-const article22ndP = document.querySelector("article:last-child p:nth-child(2n)")
+const article1 = document.querySelector("article:first-child");
+const article1P = document.querySelector("article:first-child p");
+const article2P = document.querySelector("article:last-child p");
+const article2LastP = document.querySelector("article:last-child p:last-child");
+const article22ndP = document.querySelector("article:last-child p:nth-child(2n)");
+const article1Img = document.querySelector("article img");
+const article2Img = document.querySelector("article:last-child img");
+const div1 = document.querySelector("div");
 
 // article1.textContent="lorem 2 måske?"
 
@@ -28,5 +31,12 @@ function resetAnim(){
 
 article22ndP.addEventListener("mouseover", scaleUp);
 
+article1Img.src = "https://laclosette.com/wp-content/uploads/2017/10/500x300.png";
+article2Img.src = "https://laclosette.com/wp-content/uploads/2017/10/500x300.png"
+
+// div1.innerHTML
+
+// document.createElement = "<article><img src='picsum.photos/500/300'><h2>Overskrift</h2><p>halløj</p></article>";
+document.createElement = "<article><img src='picsum.photos/500/300'><h2>Overskrift</h2><p>halløj</p></article>";
 
 
