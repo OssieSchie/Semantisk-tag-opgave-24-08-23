415e8f7aba6fd1bad7d7b5113c4d7a586624ecaf
Ossie <osvald.rye@gmail.com>
2023-08-25 12:39:52 +0200

    Shit fra undervisningen

---
+++ objektTest/book.js
@@ -0,0 +1,10 @@
+const firstBook = {
+    title: "Ringenes Herre",
+    author: "Tolkien",
+    genres: ["fantasy", "drama", "eventyr"],
+    pages: 1042,
+    hasRead: true,
+    hasIllustrations: false,
+}
+
+console.log(firstbook.genres)
